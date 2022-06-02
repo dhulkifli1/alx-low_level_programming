152d84627e9cfb04f16962a8399b5e4c7eb1c9c5
root <Dhulkifli>
2022-06-02 12:19:22 -0700

    trailing white space

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,9 +4,9 @@
 
 /**
  * main - Entry point
- *   
+ *
  * Return: Always 0 (Success)
- *     
+ *
  */
 
 
@@ -34,7 +34,7 @@ int main(void)
 
 		printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
 
-		printf("Size of a float: %lu byte(s)\n", sizeof(afloat));	
+		printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 
 																							return (0);
