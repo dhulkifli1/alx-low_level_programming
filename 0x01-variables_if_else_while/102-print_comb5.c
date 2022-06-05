c78433fdf8111023c51c584bcc2088d1e6e4760f
root <Dhulkifli>
2022-06-05 13:21:02 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: return success 0
+ */
+
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = '0'; i < '10'; i++)
+	{
+		for (j = '0'; j < '10'; j++)
+		{
+			for (k = '0'; k < '10'; k++)
+			{
+				for (l = '0'; l < '10'; l++)
+				{
+				if (i == k && j == l)
+				{
+				}
+				else
+				{
+				if ((i * '10') + j <= (k * '10') + l)
+				{
+					putchar(i + '48');
+					putchar(j + '48');
+					putchar(',');
+					putchar(k + 48);
+					putchar(l + 48);
+					if (i == 9 && j == 8 && k == 9 && l == 9)
+					{
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
+					}
+				}
+				}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
