e0b6e972eed594178a0bf7168dcc792eec87df55
root <Dhulkifli>
2022-06-05 10:35:51 -0700

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	char b;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+		for (b = '0'; b <= '9'; b++)
+		{
+			if (a != b && a < b)
+				putchar(b);
+			if (a < '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
