8bab1af4c9be3d047d6b8cadb9174fa9c7fc6e73
root <Dhulkifli>
2022-06-05 13:08:23 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (i == j && j == k)
+				{
+				}
+				else
+				{
+					if (k >= j && j >= i && k != j && j != i)
+					{
+						putchar(i);
+						putchar(j);
+						putchar(k);
+					if (i == 55 && j == 56 && k == 57)
+						{
+						}
+					else
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
