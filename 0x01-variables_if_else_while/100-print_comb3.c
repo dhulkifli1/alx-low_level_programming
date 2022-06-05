e064bf8edfa9fe9fd04e73eabe40b40aeb8a3f7b
root <Dhulkifli>
2022-06-05 12:25:38 -0700

    back to original, yet change in loop

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,14 @@ int main(void)
 	for (a = '0'; a <= '9'; a++)
 	{
 		putchar(a);
-		if (a != b && a < b && a < '8')
+	}
+	for (b = '0'; b <= '9'; b++)
+	{
+		if (a != b && a <b && a < '8')
 		{
-			for (b = '0'; b <= '9'; b++)
-			{
-				putchar(b);
-				putchar(',');
-				putchar(' ');
-			}
+			putchar(b);
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
