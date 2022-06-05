9762176271a9651d39c0247275652c1c0de1597d
root <Dhulkifli>
2022-06-05 04:10:25 -0700

    line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /*
- * main - Entry point
+ * main -Entry point
  * Return: Always 0 (Success)
  */
 
@@ -9,6 +9,7 @@ int main(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; ++c)
+
 	{
 		putchar(c);
 	}
