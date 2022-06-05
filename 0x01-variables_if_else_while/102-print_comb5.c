63baf0e82b2ab0d1fc33eee58126e873f62cf75c
root <Dhulkifli>
2022-06-05 13:32:56 -0700

    eeee

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,20 +9,20 @@ int main(void)
 {
 	int i, j, k, l;
 
-	for (i = '0'; i < '10'; i++)
+	for (i = '0'; i < 10; i++)
 	{
-		for (j = '0'; j < '10'; j++)
+		for (j = '0'; j < 10; j++)
 		{
-			for (k = '0'; k < '10'; k++)
+			for (k = '0'; k < 10; k++)
 			{
-				for (l = '0'; l < '10'; l++)
+				for (l = '0'; l < 10; l++)
 				{
 				if (i == k && j == l)
 				{
 				}
 				else
 				{
-				if ((i * '10') + j <= (k * '10') + l)
+				if ((i * 10) + j <= (k * 10) + l)
 				{
 					putchar(i + '48');
 					putchar(j + '48');
