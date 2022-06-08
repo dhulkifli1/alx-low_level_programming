025a49ecefab3387b5b635a3c1fb4200a9829036
root <Dhulkifli>
2022-06-08 05:20:56 -0700

    to

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-	char word[] = "_putchar\n";
-	int i = 0;
+	char word[] = "_putchar";
+	int i;
 
-	while (i < 9)
+	for (i = 0; i < 8; i++)
 	{
-
-		_putchar(word[i]);
-		i++;
+	_putchar(word[i]);
 	}
+	_putchar('\n');
 	return (0);
 }
