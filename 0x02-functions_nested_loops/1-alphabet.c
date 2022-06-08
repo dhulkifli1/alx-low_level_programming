c91c8810d371e7c329a65e7468ffe329f3e9cc1b
root <Dhulkifli>
2022-06-08 06:17:32 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * print_alphabet() - prints alphabet in lower case
- * Return: Always success
+ * print_alphabet - prints alphabet in order
+ * Return: 0 for success
  */
 
 void print_alphabet(void)
