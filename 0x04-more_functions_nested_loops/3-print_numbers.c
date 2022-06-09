a63cf3453b2e40c0d17638fa40cc028ee823b2e7
root <Dhulkifli>
2022-06-09 00:07:25 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers
+ * @a: character used in loop
+ * Return: Returns nothing
+ */
+
+void print_numbers(void)
+{
+	char a;
+	
+	for (a >= '0'; a <= '9'; a++)
+		_putchar(a);
+	_putchar('\n');
+}
