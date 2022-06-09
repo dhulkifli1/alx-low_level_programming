61ce2dd8c295cd5bfe01238380bdf266e59b5a47
root <Dhulkifli>
2022-06-09 00:55:15 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * @n: argument
+ * @a: used in while loop
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+
+	while (a < n)
+	{
+		if (n <= 0)
+			_putchar('\n');
+		else
+			_putchar('_');
+		a++;
+	}
+	_putchar('\n');
+}
