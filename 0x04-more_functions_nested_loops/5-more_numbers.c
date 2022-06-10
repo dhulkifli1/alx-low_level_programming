ab636fad05cece5911de8a37eea5eb19c2f4e435
root <Dhulkifli>
2022-06-10 12:59:56 -0700

    mkmkmmkmkmk

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,8 +2,6 @@
 
 /**
  * more_numbers -  prints 10 times the numbers, from 0 to 14
- * @a: While loop variable
- * @b: For loop variable
  * Return: nothing
  */
 
@@ -11,14 +9,17 @@ void more_numbers(void)
 {
 	int a = 0;
 	int b;
+	int tens;
 
-	while (a <= 57; a++)
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= '14'; b++)
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(b);
+			tens = '0' + (b / 10);
+			if (tens != '0')
+				_putchar(tens);
+			_putchar('0' + b % 10);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
