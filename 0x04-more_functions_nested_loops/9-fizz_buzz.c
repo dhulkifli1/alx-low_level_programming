676e15b318bfad3f4928f5af93422e4f3b35e730
root <Dhulkifli>
2022-06-10 13:23:08 -0700

    nhgh

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,16 +10,22 @@ int main(void)
 {
 	int a = 1;
 
-	while (a <= 100; a++)
+	while (a <= 100)
 	{
-		if (a % 51 == 48)
+		if (a % 3 == 0)
 			printf("Fizz ");
-		else if (a % 53 == 48)
+		else if (a % 5 == 0)
 			printf("Buzz ");
-		else if (a % 51 == 48 && a % 53 == 48)
+		else if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
 		else
 			printf("%d ", a);
+		if (i != 100)
+		{
+			putchar(' ');
+		}
+
+		a++;
 	}
 	return (0);
 }
