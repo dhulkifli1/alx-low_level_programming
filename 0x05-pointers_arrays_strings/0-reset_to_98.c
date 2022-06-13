dedbda23592fc6a0aafc4279324b91f0c7731689
root <Dhulkifli>
2022-06-13 05:46:36 -0700

    now then

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,15 +2,11 @@
 
 /**
  * reset_to_98 - updates value to 98
- *@n - argument
+ * @n - integer pointer to a variable to be updated
  * Return: nothing
  */
 
 void reset_to_98(int *n)
 {
-	int p;
-
-	p = 98;
-	n = &p;
 	*n = 98;
 }
