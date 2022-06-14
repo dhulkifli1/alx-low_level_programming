2cb45b12128e00c7fc66ab7f4de0b44cac54a052
root <Dhulkifli>
2022-06-14 05:17:42 -0700

    no , after the end

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,9 +12,14 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 1; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
 	_putchar('\n');
 }
