c954e95e0486e44b2a2af75c2fc442db71ac779d
root <Dhulkifli>
2022-06-14 00:57:41 -0700

    for loop

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -13,7 +13,7 @@ void puts_half(char *str)
 
 	if (strlen(str) % 2 == 0)
 	{
-		for (i > strlen(str) / 2; i <= strlen(str); i++)
+		for (i = strlen(str) / 2; i <= strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
