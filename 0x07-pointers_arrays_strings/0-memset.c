f0c69e032db1896822e02127d5e3426cc49a344d
root <Dhulkifli>
2022-06-20 11:23:15 -0700

    betty doc

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * *_memset - fills memory with a constant byte
+ * *_memset - fills the first n bytes of the memory area
+ *            pointed to by s with the constant byte b
  * @s: pointer to the memory area
  * @b: the constant byte
  * @n: number of times the constant byte will
