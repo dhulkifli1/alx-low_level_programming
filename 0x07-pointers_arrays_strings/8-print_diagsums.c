472b2040db97c61d28750ad5fd829cea355b913e
root <Dhulkifli>
2022-06-21 05:03:29 -0700

    *

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -25,22 +25,22 @@ void print_diagsums(int *a, int size)
 			{
 				if (i == j)
 				{
-					sum1 += a[i][j];
+					sum1 += *(a[i][j]);
 				}
 				else if (i + j == size - 1)
 				{
-					sum2 += a[i][j];
+					sum2 += *(a[i][j]);
 				}
 			}
 			else if (size % 2 == 0)
 			{
 				if (i == j)
 				{
-					sum1 += a[i][j];
+					sum1 += *(a[i][j]);
 				}
 				else if (i + j == size - 1)
 				{
-					sum2 += a[i][j];
+					sum2 += *(a[i][j]);
 				}
 			}
 		}
