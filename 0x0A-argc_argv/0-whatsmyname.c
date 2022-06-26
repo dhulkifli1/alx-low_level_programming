e4bd6717c983e85b224bbe3a29713b2450c72c40
root <Dhulkifli>
2022-06-26 07:08:33 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
