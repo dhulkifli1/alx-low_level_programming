8583712a69fda36927bfe692214b8bed5d251a0b
root <Dhulkifli>
2022-06-26 08:06:17 -0700

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 1 if error, O if success
+ */
+
+int main (int argc, char **argv)
+{
+	int i;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (isdigit(argv[i]) == 0)
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else if (atoi(argv[i]) >= 0)
+			{
+				sum += atoi(argv[i]);
+				printf("%d\n", sum);
+				return (0);
+			}
+		}
+	}
+}
