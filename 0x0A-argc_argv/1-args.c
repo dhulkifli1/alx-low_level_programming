072a4c5cc80a82ee5d65a6cb80a472eaf79c721b
root <Dhulkifli>
2022-06-26 07:36:36 -0700

    Betty documentation

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * main - entry point
+ * @argc: argument counter
+ * @argv: argument vector
+ * Return: 0
+ */
 int main(int argc, char **argv)
 {
 	(void)argv;
