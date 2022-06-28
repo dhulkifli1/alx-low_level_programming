0b65c2d4c8ed144c24f2eb90b990242c924e7ee8
root <Dhulkifli>
2022-06-28 07:57:51 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -12,29 +13,32 @@
 
 int main (int argc, char **argv)
 {
-	int i;
-	int sum = 0;
+	int i, j, length, sum;
+	char *ptr;
 
-	if (argc == 1)
-	{
+	if (argc < 2)
 		printf("0\n");
-		return (0);
-	}
 	else
 	{
+		sum = 0;
 		for (i = 1; i < argc; i++)
 		{
-			if (*argv[i] < '0' && *argv[i] > '9')
+			ptr = argv[i];
+			length = strlen(ptr);
+
+			for (j = 0; j < length; j++)
 			{
-				printf("Error\n");
-				return (1);
+				if (isdigit(*(ptr + j)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
-			else if (atoi(argv[i]) >= 0)
-			{
-				sum += atoi(argv[i]);
-				printf("%d\n", sum);
-				return (0);
-			}
+
+			sum += atoi(argv[i]);
 		}
+
+		printf("%d\n", sum);
 	}
+	return (0);
 }
