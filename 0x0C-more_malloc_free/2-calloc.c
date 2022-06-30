8d013ac3b7e5850b72832a2b9e198f7cb936cb20
root <Dhulkifli>
2022-06-30 11:21:56 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,5 +11,22 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	calloc(nmemb, size);
+	void *mry;
+	char fill;
+	unsigned int i;
+
+	if ( nmemb == 0 || size == 0)
+		return (NULL);
+
+	mry = malloc(nmemb * size);
+
+	if (mry == 0)
+		return (NULL);
+
+	fill = mry;
+
+	for (i = 0; i < (size * nmemb); i++)
+		fill[i] = '\0';
+
+	return (mry);
 }
