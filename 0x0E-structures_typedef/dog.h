dba6672f19dbc1bbf15e4bb98066263f2904d5ec
root <Dhulkifli>
2022-07-04 07:54:02 -0700

    include guarded

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,6 @@
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - struct carrying info of a dog
  * @name: dog's name
@@ -11,3 +14,15 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
