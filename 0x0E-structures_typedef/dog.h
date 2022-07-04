dd0d4397ca2935fcaae94b3b790871a96f78d95c
root <Dhulkifli>
2022-07-04 07:42:45 -0700

    Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
+/**
+ * struct dog - struct carrying info of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of the dog
+ */
+
 struct dog
 {
 	char *name;
-	float age;
 	char *owner;
+	float age;
 };
