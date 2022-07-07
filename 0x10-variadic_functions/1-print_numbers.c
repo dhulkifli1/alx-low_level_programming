5edaa9011a086be14a6e7a15d078c87d3039807d
root <Dhulkifli>
2022-07-07 12:29:58 -0700

    print in single line

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,9 +20,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (separator != NULL)
-			printf("%d%s\n", va_arg(ap, int), separator);
+			printf("%d%s", va_arg(ap, int), separator);
 		else
-			printf("%d\n", va_arg(ap, int));
+			printf("%d", va_arg(ap, int));
 	}
+	printf("\n");
 	va_end(ap);
 }
