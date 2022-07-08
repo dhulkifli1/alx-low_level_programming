7994e3a00ad17ac6c53609682367f6c9ffc75899
root <Dhulkifli>
2022-07-08 06:01:54 -0700

    undefined references

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,26 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
+#include <stdarg.h>
+
+/**
+ * charStructures - Structure holding symbol and func
+ * @symbol: var type
+ * @print: Function
+ */
+typedef struct charStructures
+{
+	char *varRep;
+	void (*print)(va_list);
+} charStructures;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+void printChar(va_list arg);
+void printInt(va_list arg);
+void printFloat(va_list arg);
+void printString(va_list arg);
 void print_all(const char * const format, ...);
 int _putchar(int c);
 
