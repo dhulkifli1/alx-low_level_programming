154c4b2bdebfc6608f13b323e18d96400f4579b9
root <Dhulkifli>
2022-07-21 08:11:11 -0700

    1
    :x
    
    0-binary_to_uint.c
    git commit -m 0-binary_to_uint.c
    0
    1
    10

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,30 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/**
+ * struct power_properties - Power properties
+ * @value: the value
+ * @power: The power
+ * Description: Not available
+ */
+typedef struct power_properties
+{
+	unsigned long int value;
+	int power;
+} pwrProp;
+
+long int power(int base, unsigned int pow);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+pwrProp getMaxPower(int value);
+int _putchar(char c);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
+
+#endif
