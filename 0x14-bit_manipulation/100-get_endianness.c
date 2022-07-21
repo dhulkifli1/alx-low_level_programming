a31dc64846d04662343dda8db131fe3bb4b6fcca
root <Dhulkifli>
2022-07-21 11:11:23 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - Get the endianness of pc
+ *
+ * Return: 0 if big endian or 1 otherwise
+ */
+int get_endianness(void)
+{
+	unsigned long int a = 0x41FFFF42;
+	char *b = (char *)&a;
+	if (b[0] == 'A')
+		return (0);
+	else
+		return (1);
+}
