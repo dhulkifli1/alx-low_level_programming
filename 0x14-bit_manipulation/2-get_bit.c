56b4e7b2c38dd3fad7b1264f09d12bc043401711
root <Dhulkifli>
2022-07-21 08:18:03 -0700

    2-get_bit.c

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * get_bit - Get the bit object at an index
+ *
+ * @n: The value
+ * @index: Index of the bit
+ * Return: (int) The bit
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	return ((n >> index) & 1);
+}
